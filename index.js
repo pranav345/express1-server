75fc06bb722b5e264fdf3e5cd831c2df6fbf15ea
Pranav Mishra <pranav.mishra.met17@itbhu.ac.in>
2021-02-22 22:31:16 +0530

    express1

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var app = express();
+var port = process.env.PORT||'3000';
+var hostname = 'localhost';
+
+app.use(express.static(__dirname+"/public"));
+app.use(morgan('dev'));
+app.use((req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end("<html><body><h1>heyy there I have created a server</h1></body></html>");
+})
+
+const server = http.createServer(app);
+server.listen(port,hostname,()=>{
+    console.log('server started');
+})
+
